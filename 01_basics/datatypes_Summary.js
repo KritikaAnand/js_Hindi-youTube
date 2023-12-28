db39d269f4d550e2488edbb55e0ad71cc32e160e
Kritika Anand <96490053+KritikaAnand@users.noreply.github.com>
2023-12-28 07:52:48 +0000

    datatype summary

---
+++ 01_basics/datatypes_Summary.js
@@ -0,0 +1,23 @@
+const id = Symbol("123")
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);     //false, both are different i.e unique
+
+const bigNumber = 387546874598349067874567n  // bigInt
+
+
+//******************* ARRAY, OBJECT, FUNCTIONS ********************
+
+const heros =["shaktiman", "ironman", "spiderman"]
+{
+    name: "kritika"             //objects are written in curly braces.
+    age: 20
+
+}
+
+//*****functions */
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
