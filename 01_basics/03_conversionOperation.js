6479188e7a1a653ab08de238b122d38ee254f1c0
Kritika Anand <96490053+KritikaAnand@users.noreply.github.com>
2023-12-28 07:23:42 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,3 +15,18 @@ console.log(valueInMarks);
 //"33" => 33 
 //"33abc" => NaN(not-a-number)
 //true => 1 , false => 0 , "" => false , "kittu" => true
+
+
+// *********************** OPERATIONS ********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = "kritika"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(+true);
+console.log(+"");
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+console.log(2 > 1);
+
+//when you compare make sure that both having same datatype
+
+console.log(null > 0);   //false
+console.log(null == 0);  //false
+console.log(null >= 0);  //true
+
+// === it checks the values strictly as well as the datatypes
+
+console.log("2" === 2);
