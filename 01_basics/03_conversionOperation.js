893c16d1260d2ceeae1bb6b9fdcf0f859fb45ab6
Kritika Anand <96490053+KritikaAnand@users.noreply.github.com>
2023-12-28 06:20:31 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof score);
+console.log(valueInNumber);
+
+let marks = null
+let valueInMarks = Number(marks)
+console.log(typeof null);
+console.log(marks);
+console.log(valueInMarks);
+
+//"33" => 33 
+//"33abc" => NaN(not-a-number)
+//true => 1 , false => 0 , "" => false , "kittu" => true
